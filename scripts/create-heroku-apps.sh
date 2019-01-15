f2e493bb3d676bb2093d9ae5d24fcb865fa68ee2
Simon Oulevay <simon.oulevay@gmail.com>
2019-01-15 13:27:08 +0100

    Optional shorter IDs in CSV column 3 to fit within Heroku's 30-character limit

--- scripts/create-heroku-apps.sh
+++ scripts/create-heroku-apps.sh
@@ -29,6 +29,7 @@ fail() {
   exit 1
 }
 
+# Parse options
 while [[ $# -gt 0 ]]; do
   key="$1"
   [[ "$key" =~ ^- ]] || break
@@ -51,12 +52,14 @@ done
 
 csv_file="$1"
 
+# Make sure CSV file is named *.csv and exists
 if test -z "$csv_file" || ! [[ "$csv_file" =~ \.csv$ ]]; then
   fail "First argument must be a CSV file"
 elif ! test -f "$csv_file"; then
   fail "No such file: $csv_file"
 fi
 
+# Read CSV file
 echo -n "Reading $(color $BOLD "$csv_file")..."
 csv=`cat "$csv_file"|grep @`
 echo " $(color $GREEN done)"
@@ -68,25 +71,43 @@ for student in $csv; do
   IFS=$','; declare -a fields=($student)
   IFS="$OLD_IFS"
 
-  name="${fields[0]}"
-  email="${fields[1]}"
+  # Column 0: human-readable name
+  name="$(echo "${fields[0]}"|tr -d '[:space:]')"
+
+  # Column 1: email address
+  email="$(echo "${fields[1]}"|tr -d '[:space:]')"
+
+  # Column 2 (optional): Heroku account email address (if different)
   heroku_email="$(echo "${fields[2]}"|tr -d '[:space:]')"
   id="$(echo "$email"|sed 's/@.*//'|sed 's/\./-/')"
 
+  # Column 3 (optional): custom ID (useful if default ID is too long)
+  # The default ID is the name portion of the email address,
+  # with dots replaced by hyphens.
+  short_id="$(echo "${fields[3]}"|tr -d '[:space:]')"
+  if test -n "$short_id"; then
+    id="$short_id"
+  fi
+
+  # Default Heroku account email address to column 1 email
   if test -z "$heroku_email"; then
     heroku_email="$(echo "$email"|tr -d '[:space:]')"
   fi
 
-  heroku_app="comem-archidep-${id}"
+  # Build app name from prefix ("ad"), ID and optional suffix (-n|--name option)
+  heroku_app="ad-${id}"
   if test -n "$app_name"; then
     heroku_app="${heroku_app}-${app_name}"
   fi
 
+  # Skip other IDs if -o|--only option is specified
   [[ "${#only[@]}" -gt 0 ]] && ! contains "$id" "${only[@]}" && continue
 
+  # Print header with human-readable name
   echo
   echo "$(color $BOLD [${name}])"
 
+  # Create the Heroku application
   echo -n "Checking if app $(color $BOLD "$heroku_app") exists..."
   if heroku apps:info -a "$heroku_app" &>/dev/null; then
     echo " $(color $GREEN yes)"
@@ -102,6 +123,7 @@ for student in $csv; do
     fi
   fi
 
+  # Add the Heroku account email as a collaborator (if the -c|--collaborators option is specified)
   if [[ $configure_collaborator -eq 1 ]]; then
     echo -n "Checking if $(color $BOLD "$heroku_email") is a collaborator..."
     if heroku access -a "$heroku_app"|grep "$heroku_email" &>/dev/null; then
