9c2570b666f0c087d322913a9c812d4382fa5acd
Simon Oulevay <simon.oulevay@gmail.com>
2024-10-07 21:55:11 +0200

    Adapt script to check student server

--- scripts/check-student-server.sh
+++ scripts/check-student-server.sh
@@ -9,9 +9,9 @@ STUDENT_USER="$2"
 test -n "$STUDENT_IP" || fail "The IP address of the machine to check must be provided to this script as the first argument"
 test -n "$STUDENT_USER" || fail "The username of the account to connect as must be provided to this script as the second argument"
 
-scp -i id_rsa scripts/listen-server-ports.sh ${STUDENT_USER}@$STUDENT_IP:/home/${STUDENT_USER}/listen-server-ports.sh || fail "Could not copy listen-server-ports.sh script to server"
-ssh -i id_rsa ${STUDENT_USER}@$STUDENT_IP chmod 755 /home/${STUDENT_USER}/listen-server-ports.sh || fail "Could not set permissions of listen-server-ports.sh script on server"
-ssh -i id_rsa ${STUDENT_USER}@$STUDENT_IP "sudo nohup /home/${STUDENT_USER}/listen-server-ports.sh &>/dev/null < /dev/null &" || fail "Could not execute listen-server-ports.sh script on server"
+scp -i id_archidep scripts/listen-server-ports.sh ${STUDENT_USER}@$STUDENT_IP:/home/${STUDENT_USER}/listen-server-ports.sh || fail "Could not copy listen-server-ports.sh script to server"
+ssh -i id_archidep ${STUDENT_USER}@$STUDENT_IP chmod 755 /home/${STUDENT_USER}/listen-server-ports.sh || fail "Could not set permissions of listen-server-ports.sh script on server"
+ssh -i id_archidep ${STUDENT_USER}@$STUDENT_IP "sudo nohup /home/${STUDENT_USER}/listen-server-ports.sh &>/dev/null < /dev/null &" || fail "Could not execute listen-server-ports.sh script on server"
 
 echo
 echo "Waiting 3 seconds for script to launch..."
@@ -23,7 +23,7 @@ for port in 80 443 3000 3001; do
   echo $port; curl --connect-timeout 3 --fail-with-body "http://$STUDENT_IP:$port/test.txt" && echo || echo NOK
 done
 
-ssh -i id_rsa ${STUDENT_USER}@$STUDENT_IP sudo killall listen-server-ports.sh
+ssh -i id_archidep ${STUDENT_USER}@$STUDENT_IP sudo killall listen-server-ports.sh
 
 echo
 for port in 80 443 3000 3001; do
@@ -31,18 +31,18 @@ for port in 80 443 3000 3001; do
   echo
 done
 
-ssh -i id_rsa ${STUDENT_USER}@$STUDENT_IP rm -f /home/${STUDENT_USER}/listen-server-ports.sh
+ssh -i id_archidep ${STUDENT_USER}@$STUDENT_IP rm -f /home/${STUDENT_USER}/listen-server-ports.sh
 
 echo
-ssh -i id_rsa ${STUDENT_USER}@$STUDENT_IP "sudo ls -laR /home/*/.ssh"
+ssh -i id_archidep ${STUDENT_USER}@$STUDENT_IP "sudo ls -laR /home/*/.ssh"
 
 echo
-ssh -i id_rsa ${STUDENT_USER}@$STUDENT_IP hostname
-ssh -i id_rsa ${STUDENT_USER}@$STUDENT_IP cat /etc/hostname
+ssh -i id_archidep ${STUDENT_USER}@$STUDENT_IP hostname
+ssh -i id_archidep ${STUDENT_USER}@$STUDENT_IP cat /etc/hostname
 
 echo
-ssh -i id_rsa ${STUDENT_USER}@$STUDENT_IP ls -lah /swapfile || echo "Swap file not found"
+ssh -i id_archidep ${STUDENT_USER}@$STUDENT_IP ls -lah /swapfile || echo "Swap file not found"
 echo
-ssh -i id_rsa ${STUDENT_USER}@$STUDENT_IP cat /etc/fstab
+ssh -i id_archidep ${STUDENT_USER}@$STUDENT_IP cat /etc/fstab
 echo
-ssh -i id_rsa ${STUDENT_USER}@$STUDENT_IP free -h
+ssh -i id_archidep ${STUDENT_USER}@$STUDENT_IP free -h
